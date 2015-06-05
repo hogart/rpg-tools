ee96e22b3d63bd4f4aea57d2c3fc8610f4e033fa
Konstantin Kitmanov <doctor.hogart@gmail.com>
2015-06-05 15:55:27 +0700

    starting tests for inventory

--- test/inventory.test.js
+++ test/inventory.test.js
@@ -11,4 +11,31 @@ describe('inventory', function () {
 		assert.isFunction(inventory.equipFromInventory);
 		assert.isFunction(inventory.isWearable);
     });
+
+    describe('uneqip', function () {
+        it('removes item from given slot and pushes it to inventory', function () {
+            var helmet = {
+                slot: 'head'
+            };
+
+            var attributes = {
+                equipped: {
+                    head: helmet
+                },
+                inventory: []
+            };
+
+            inventory.unEquip(attributes, 'head');
+
+            assert.lengthOf(attributes.inventory, 1);
+            assert.equal(attributes.inventory[0], helmet);
+            assert.isNull(attributes.equipped.head);
+
+            assert.throws(
+                inventory.unEquip.bind(null, attributes, 'non-existent slot name'),
+                /No such slot: non-existent slot name/,
+                'throws exception when non-existent slot'
+            );
+        });
+    });
 });
