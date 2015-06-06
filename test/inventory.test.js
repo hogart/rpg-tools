069ebc27607c620ecb3191e873b3f80f3f7d278e
Konstantin Kitmanov <doctor.hogart@gmail.com>
2015-06-06 20:13:12 +0700

    added inventory.equip tests

--- test/inventory.test.js
+++ test/inventory.test.js
@@ -16,7 +16,6 @@ describe('inventory', function () {
         var helmet = {
             slot: 'head'
         };
-
         var attributes = {
             equipped: {
                 head: helmet,
@@ -24,9 +23,15 @@ describe('inventory', function () {
             },
             inventory: []
         };
+        var unEquip = inventory.unEquip;
+
+        it('returns same object it was given', function () {
+            var newAttribs = unEquip(attributes, 'emptySlot');
+            assert.ok(newAttribs === attributes, 'same object');
+        });
 
         it('removes item from given slot and pushes it to inventory', function () {
-            inventory.unEquip(attributes, 'head');
+            unEquip(attributes, 'head');
 
             assert.lengthOf(attributes.inventory, 1);
             assert.equal(attributes.inventory[0], helmet);
@@ -36,7 +41,7 @@ describe('inventory', function () {
         it('does not changes anything when working with empty slot', function () {
             var inventoryLength = attributes.inventory.length;
 
-            inventory.unEquip(attributes, 'emptySlot');
+            unEquip(attributes, 'emptySlot');
 
             assert.lengthOf(attributes.inventory, inventoryLength);
             assert.isNull(attributes.equipped.emptySlot);
@@ -44,7 +49,7 @@ describe('inventory', function () {
 
         it('throws specific error with incorrect slot', function () {
             assert.throws(
-                inventory.unEquip.bind(null, attributes, 'non-existent slot name'),
+                unEquip.bind(null, attributes, 'non-existent slot name'),
                 /No such slot: non-existent slot name/,
                 'throws exception when non-existent slot'
             );
@@ -52,11 +57,39 @@ describe('inventory', function () {
     });
 
     describe('equipFromInventory', function () {
-        it('removes item from inventory and places it to correct slot', function () {
-            var helmet = {
-                slot: 'head'
-            };
+        var equip = inventory.equipFromInventory;
+        var helmet = {
+            slot: 'head'
+        };
+        var heavyHelmet = {
+            slot: 'head',
+            name: 'heavy helmet',
+            requirements: {
+                str: { min: 10 }
+            }
+        };
+        var lightHelmet = {
+            slot: 'head',
+            name: 'light helmet',
+            requirements: {
+                str: { min: 0 }
+            }
+        };
 
+        it('returns same object it was given', function () {
+            var attributes = {
+                equipped: {
+                    head: null,
+                    emptySlot: null
+                },
+                inventory: [helmet]
+            };
+            var newAttribs = equip(attributes, 0, 'head');
+
+            assert.ok(newAttribs === attributes, 'same object');
+        });
+
+        it('removes item from inventory and places it to correct slot', function () {
             var attributes = {
                 equipped: {
                     head: null,
@@ -65,10 +98,121 @@ describe('inventory', function () {
                 inventory: [helmet]
             };
 
-            inventory.equipFromInventory(attributes, 0, 'head');
+            equip(attributes, 0, 'head');
 
             assert.lengthOf(attributes.inventory, 0);
             assert.equal(attributes.equipped.head, helmet);
         });
+
+        it('throws error on invalid index and does not change inventory and equipped', function () {
+            var attributes = {
+                equipped: {
+                    head: null,
+                    emptySlot: null
+                },
+                inventory: [helmet]
+            };
+
+            assert.throws(
+                equip.bind(null, attributes, 1, 'head'),
+                /Invalid inventory index: 1/,
+                'no such index thrown'
+            );
+
+            assert.deepEqual(attributes.equipped, {head: null, emptySlot: null});
+            assert.deepEqual(attributes.inventory, [helmet]);
+        });
+        
+        it('throws error on invalid slotName and does not change inventory and equipped', function () {
+            var badItem = {
+                slot: 'wtf', name: 'wtf'
+            };
+            var attributes = {
+                equipped: {
+                    head: null,
+                    emptySlot: null
+                },
+                inventory: [helmet, badItem]
+            };
+
+            assert.throws(
+                equip.bind(null, attributes, 0, 'invalid slot'),
+                /No such slot: invalid slot/,
+                'no such slot thrown'
+            );
+
+            assert.deepEqual(attributes.equipped, {head: null, emptySlot: null});
+            assert.deepEqual(attributes.inventory, [helmet, badItem]);
+
+            assert.throws(
+                equip.bind(null, attributes, 1),
+                /No such slot: wtf/,
+                'no such slot thrown when slot was auto-selected'
+            );
+
+            assert.deepEqual(attributes.equipped, {head: null, emptySlot: null});
+            assert.deepEqual(attributes.inventory, [helmet, badItem]);
+        });
+        
+        it('throws error when item.slot and slotName mismatch', function () {
+            var attributes = {
+                equipped: {
+                    head: null,
+                    feet: null
+                },
+                inventory: [helmet]
+            };
+
+            assert.throws(
+                equip.bind(null, attributes, 0, 'feet'),
+                /Item intended for head and can not be equipped to feet/,
+                'slot mismatch thrown'
+            );
+
+            assert.deepEqual(attributes.equipped, {head: null, feet: null});
+            assert.deepEqual(attributes.inventory, [helmet]);
+        });
+
+        it('throws error when item requirements not met and vice versa', function () {
+            var attributes = {
+                str: 1,
+                equipped: {
+                    head: null
+                },
+                inventory: [heavyHelmet, lightHelmet]
+            };
+
+            assert.throws(
+                equip.bind(null, attributes, 0),
+                /Item requirements not met/,
+                'requirements exception thrown'
+            );
+
+            assert.deepEqual(attributes.equipped, {head: null});
+            assert.deepEqual(attributes.inventory, [heavyHelmet, lightHelmet]);
+
+            assert.doesNotThrow(
+                equip.bind(null, attributes, 1),
+                /Item requirements not met/,
+                'requirements exception NOT thrown'
+            );
+
+            assert.deepEqual(attributes.equipped, {head: lightHelmet});
+            assert.deepEqual(attributes.inventory, [heavyHelmet]);
+        });
+
+        it('unequips item if slot is occupied', function () {
+            var attributes = {
+                str: 15,
+                equipped: {
+                    head: lightHelmet
+                },
+                inventory: [heavyHelmet]
+            };
+
+            equip(attributes, 0);
+            assert.deepEqual(attributes.equipped, {head: heavyHelmet});
+            assert.deepEqual(attributes.inventory, [lightHelmet]);
+        })
     });
 });
