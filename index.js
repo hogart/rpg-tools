5e243472d5d88e44fde32aa639533a55fe7cb5c4
Konstantin Kitmanov <doctor.hogart@gmail.com>
2015-06-07 20:32:54 +0700

    ProtoTree tests, refactoring, optimization

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
     'use strict';
     /* global define, module, require */
     if (typeof define === 'function' && define.amd) { // AMD
-        define(['./lib/inventory', './lib/modifiers', './lib/ptp', './lib/random', './lib/requirements', './lib/utils'], factory);
+        define(['./lib/inventory', './lib/modifiers', './lib/ProtoTree', './lib/random', './lib/requirements', './lib/utils'], factory);
     } else if (typeof exports === 'object') { // Node, browserify and alike
-        module.exports = factory(require('./lib/inventory'), require('./lib/modifiers'), require('./lib/ptp'), require('./lib/random'), require('./lib/requirements'), require('./lib/utils'));
+        module.exports = factory(require('./lib/inventory'), require('./lib/modifiers'), require('./lib/ProtoTree'), require('./lib/random'), require('./lib/requirements'), require('./lib/utils'));
     } else { // Browser globals (root is window)
         root.rpgTools = (root.rpgTools || {});
         root.rpgTools = factory(root.rpgTools.inventory, root.rpgTools.modifiers, root.rpgTools.ptp, root.rpgTools.random, root.rpgTools.requirements, root.rpgTools.utils);
     }
-}(this, function (inventory, modifiers, ptp, random, requirements, utils) {
+}(this, function (inventory, modifiers, ProtoTree, random, requirements, utils) {
     'use strict';
 
     var exports = {
         inventory: inventory,
         modifiers: modifiers,
-        ptp: ptp,
+        ProtoTree: ProtoTree,
         random: random,
         requirements: requirements
     };
---
+++ test/ProtoTree.test.js
@@ -0,0 +1,145 @@
+/* eslint-env mocha */
+
+'use strict';
+
+var ProtoTree = require('../lib/ProtoTree');
+var assert = require('chai').assert;
+var weaponTree = {
+    '@weapon': {
+        name: '-- Weapon --',
+        description: 'If you see this, please contact developers.',
+        type: 'weapon',
+        icon: 'generic-weapon'
+    },
+
+    '@sword': {
+        proto: '@weapon',
+        name: '-- Sword --',
+        slot: 'rightHand',
+        icon: 'generic-sword'
+    },
+
+    falchion: {
+        proto: '@sword',
+        name: 'Falchion',
+        description: 'Falchion have one-sided blade.',
+        requirements: {
+            'class': 'warrior'
+        },
+        ar: 10
+    },
+
+    twoHandedSword: {
+        proto: '@sword',
+        isTwoHanded: true,
+        name: 'Two-handed sword',
+        description: 'Very long and heavy sword',
+        requirements: {
+            'class': 'warrior'
+        },
+        ar: 15
+    },
+
+    flamberg: {
+        proto: 'twoHandedSword',
+        name: 'Flamberg',
+        description: 'Flamberg is so-called flaming sword, with wavy blade.',
+        requirements: {
+            'class': 'warrior'
+        },
+        ar: 20
+    },
+
+    espada: {
+        proto: '@sword',
+        name: 'Espada',
+        description: 'Espada',
+        requirements: {
+            'class': 'scout'
+        },
+        ar: 10,
+        icon: 'generic-espada'
+    },
+
+    rapier: {
+        proto: 'espada',
+        name: 'Rapier',
+        description: 'Rapier.',
+        requirements: {
+            'class': 'scout'
+        },
+        ar: 15,
+        icon: 'rapier'
+    },
+
+    gladius: {
+        proto: '@sword',
+        name: 'Gladius',
+        description: 'Short sword, lightweight and handy in close quarters.',
+        requirements: {
+            'class': 'bard'
+        },
+        ar: 10
+    },
+
+    invalid: {
+        name: 'Error',
+        proto: '@error'
+    }
+};
+
+describe('ProtoTree', function () {
+    var protoTree;
+    beforeEach(function () {
+        protoTree = new ProtoTree(weaponTree);
+    });
+
+    it('has `get` method', function () {
+        assert.isFunction(protoTree.get);
+    });
+    
+    it('returns items by name', function () {
+        var falchion = protoTree.get('falchion');
+
+        assert.equal(falchion.name, 'Falchion');
+        assert.equal(falchion.description, 'Falchion have one-sided blade.');
+        assert.equal(falchion.ar, 10);
+        assert.equal(falchion.slot, 'rightHand');
+        assert.equal(falchion.icon, 'generic-sword');
+        assert.equal(falchion.type, 'weapon');
+        assert.deepEqual(falchion.requirements, {class: 'warrior'});
+
+        assert.notProperty(falchion, 'proto deleted');
+
+        var rapier = protoTree.get('rapier');
+        assert.deepEqual(rapier, {
+            name: 'Rapier',
+            description: 'Rapier.',
+            requirements: {
+                class: 'scout'
+            },
+            ar: 15,
+            icon: 'rapier',
+            type: 'weapon',
+            slot: 'rightHand'
+        }, '3 level inheritance successful');
+
+        assert.notProperty(falchion, 'proto', 'proto deleted');
+    });
+    
+    it('throws error when non-existent item is requested', function () {
+        assert.throws(
+            protoTree.get.bind(protoTree, 'wtf'),
+            /No such object in given tree as wtf/,
+            'correct exception on incorrect name'
+        );
+    });
+    
+    it('throws error when item with invalid proto requested', function () {
+        assert.throws(
+            protoTree.get.bind(protoTree, 'invalid'),
+            /Invalid proto specified for Error/,
+            'correct exception on invalid item'
+        );
+    })
+});
