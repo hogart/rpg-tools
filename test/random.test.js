e2b5a04bbe815f480e317ddcea1a176ff9522ad6
Konstantin Kitmanov <hogart@coccoc.vn>
2015-06-12 18:35:39 +0700

    added some random methods and tests

--- test/random.test.js
+++ test/random.test.js
@@ -7,6 +7,7 @@ var assert = require('chai').assert;
 
 describe('random', function () {
     it('has methods', function () {
+        assert.isFunction(random.int);
         assert.isFunction(random.key);
         assert.isFunction(random.item);
     });
@@ -19,6 +20,47 @@ describe('random', function () {
     var arr = Object.keys(obj);
     var values = arr.map(function (key) {
         return obj[key];
+    });
+
+    describe('random.int', function () {
+        it('returns integer numbers in proper range', function () {
+            var rand;
+            for (var i = 0; i < 10; i++) {
+                rand = random.int(5, 10);
+                assert.equal(rand, Math.round(rand));
+                assert.isBelow(rand, 10);
+                assert.isAbove(rand, 4);
+            }
+        });
+
+        it('defaults to 0 if one argument is omitted', function () {
+            var rand;
+            for (var i = 0; i < 10; i++) {
+                rand = random.int(5);
+                assert.isBelow(rand, 5);
+                assert.isAbove(rand, -1);
+            }
+        });
+    });
+
+    describe('random.sign', function () {
+        it('returns only -1 or 1, no other number', function () {
+            var hadPlus = false;
+            var hadMinus = false;
+            var sign;
+            for (var i = 0; i < 10; i++) {
+                sign = random.sign();
+                assert.include([-1, 1], sign);
+                if (sign > 0) {
+                    hadPlus = true;
+                } else if (sign < 0) {
+                    hadMinus = true;
+                }
+            }
+
+            assert.ok(hadMinus, 'there were some minuses');
+            assert.ok(hadPlus, 'but some pluses also');
+        });
     });
 
     describe('random.key', function () {
