244f700a367d7c7b296a4c3dc633ae74294fb7df
Konstantin Kitmanov <doctor.hogart@gmail.com>
2015-07-01 19:30:00 +0300

    better test for modifiers.apply

--- test/modifiers.test.js
+++ test/modifiers.test.js
@@ -7,9 +7,22 @@ var modifiers = require('../lib/modifiers');
 
 describe('modifiers', function () {
     describe('apply', function () {
+        it('if modifier is falsy, return number as is', function () {
+            assert.equal(modifiers.apply(12, 0), 12, '0 ok');
+            assert.equal(modifiers.apply(12, null), 12, 'null ok');
+            assert.equal(modifiers.apply(12, undefined), 12, 'undefined ok');
+            assert.equal(modifiers.apply(12, ''), 12, 'empty string ok');
+            assert.equal(modifiers.apply(12, NaN), 12, 'NaN ok');
+        });
+
         it('properly adds/substracts number', function () {
             assert.equal(modifiers.apply(4, 2), 6, 'added number ok');
             assert.equal(modifiers.apply(4, -2), 2, 'substracted number ok');
+        });
+
+        it('properly adds/substracts string', function () {
+            assert.equal(modifiers.apply(4, '2'), 6, 'added string ok');
+            assert.equal(modifiers.apply(4, '-2'), 2, 'substracted string ok');
         });
 
         it('properly works with percents', function () {
